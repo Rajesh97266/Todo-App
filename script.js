54a65d6460716222c4e67556a3057c3e90bfacf9
Rajesh97266 <rajesh97266@gmail.com>
2024-10-10 20:23:06 +0530

    second

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ function createTodoItem(todo, todoIndex) {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               height="24px"
+              
               viewBox="0 -960 960 960"
               width="24px"
               fill="transparent"
